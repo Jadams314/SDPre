01f877f035861c42d2c729ef9f0bc87e3abd4055
Jadams <Justin.adams314@gmail.com>
2020-12-04 16:51:33 -0600

    Completed Objective2Lab3

---
+++ labs/Objective2Lab3.java
@@ -0,0 +1,8 @@
+public class Objective2Lab3 {
+  public static void main(String [] args){
+    String myVariable = "Motorcycles";
+    System.out.println (myVariable);
+    myVariable = "Airplanes";
+    System.out.println(myVariable);
+  }
+}
