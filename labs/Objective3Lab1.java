8d22e1b6416364822edf13bc2982e1ed869917c2
Jadams <Justin.adams314@gmail.com>
2020-12-04 18:19:28 -0600

    Completed Objective3Lab1

---
+++ labs/Objective3Lab1.java
@@ -0,0 +1,10 @@
+public class Objective3Lab1 {
+  public static void main(String [] args){
+    double num1, num2, num3, average;
+      num1=10;
+      num2=12;
+      num3=14;
+      average =(num1+num2+num3)/3;
+      System.out.println(average);
+  }
+}
