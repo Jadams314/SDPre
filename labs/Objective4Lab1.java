eee25876615d5f0c1eecb0dc7d7507df738e907b
Jadams <Justin.adams314@gmail.com>
2020-12-05 13:05:06 -0600

    Completed Objective4Lab1

---
+++ labs/Objective4Lab1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Objective4Lab1 {
+  public static void main(String[] args) {
+
+    java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+    System.out.println("What is your first name?");
+    String fname = scanner.nextLine();
+    System.out.println("What is your last name?");
+    String lname = scanner.nextLine();
+    System.out.println("What is your favorite animal?");
+    String favoriteAnimal = scanner.nextLine();
+    System.out.println("What is your favorite food?");
+    String favoriteFood = scanner.nextLine();
+    System.out.println("What is your favorite song?");
+    String favoriteSong = scanner.nextLine();
+
+    System.out.println("My fist name is "+fname);
+    System.out.println("My last name is "+lname);
+    System.out.println("My favorite animal is "+favoriteAnimal);
+    System.out.println("My favorite food is "+favoriteFood);
+    System.out.println("My favorite song is "+favoriteSong);
+
+    scanner.close();
+  }
+}
