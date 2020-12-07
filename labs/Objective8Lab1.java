43c8e31f102f82b3daa5a39a4cd4d8c03b29c4b7
Jadams <Justin.adams314@gmail.com>
2020-12-07 08:33:50 -0600

    Completed Objective8Lab1

---
+++ labs/Objective8Lab1.java
@@ -0,0 +1,12 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    int count = + 1;
+
+    for (count = 1; count<= 10; count++){
+      System.out.println(count);
+    }
+    for (count = 10; count>0; count --){
+      System.out.println(count);
+    }
+  }
+}
