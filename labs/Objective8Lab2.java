88ac71827bdf9221074ea8aab2b88fbc59cd71a9
Jadams <Justin.adams314@gmail.com>
2020-12-07 08:48:53 -0600

    Completed Objective8Lab2

---
+++ labs/Objective8Lab2.java
@@ -0,0 +1,12 @@
+public class Objective8Lab2{
+  public static void main(String[] args) {
+    int count = + 1;
+    int sum = 0;
+
+    for (count = 0; count <= 20; count++){
+      sum += count;
+    }
+    System.out.println(sum);
+
+  }
+}
