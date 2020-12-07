e28f9b859ecacd34688efd697111c7e3c609bb85
Jadams <Justin.adams314@gmail.com>
2020-12-07 09:10:07 -0600

    Completed Objective8Lab3

---
+++ labs/Objective8Lab3.java
@@ -0,0 +1,15 @@
+public class Objective8Lab3 {
+  public static void main(String[] args) {
+    int count = 1;
+
+    for (count = 1; count<=20; count++){
+      if (count % 2 > 0){
+        System.out.println(count + " is odd.");
+      }
+      else {
+        System.out.println(count + " is even.");
+
+      }
+    }
+  }
+}
